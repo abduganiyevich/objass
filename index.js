d1591dde451d5704212df3d388c4154576b04240
abduganiyevich <abduganiyevyahyobek555@gmail.com>
2023-10-27 18:30:38 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,58 @@
+// 1-misol
+
+// let array=[1,2,3,4,5,6,7,8,9];
+// function getOddNumbers(array) {
+//   return array.filter(number => number % 2 == 1);
+// }
+// console.log(getOddNumbers(array));
+
+
+
+// 2-misol
+
+// let array=[1,2,3,,5,6,7,8,9];
+// function getOddNumbers(array) {
+//   return array.map((number)=>number**3);
+// }
+// console.log(getOddNumbers(array));
+
+
+
+
+// 3-misol
+
+
+// let array=[1,2,3,4,5,6,7,8,9,45];
+// function getOddNumbers(array) {
+//   return array.some((number)=>{
+//     return number%5==0&&number%3==0;
+//   });
+// }
+// localStorage.setItem("number",getOddNumbers(array));
+
+
+// 4-misol
+
+// let array=["oma","anor","nok","behi","kiwi","ananas","mango"];
+// function newArr(array) {
+//   return array.map(entry => {
+//     const words = entry.split(' ');
+//     const initials = words.map(word => word.charAt(0).toUpperCase());
+//     return initials.join('');
+//   });
+// }
+
+// console.log(newArr(array));
+
+
+// 5-misol
+
+
+
+// 6-misol
+
+// let arr="salom bolalar";
+// function newWord(arr){
+//   return arr.replace("r","p")
+// };
+// console.log(newWord(arr));
